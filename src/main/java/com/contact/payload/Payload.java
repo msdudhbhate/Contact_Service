befdeb6afc1b65db562c7e7ebdd41e3b2599fa3b
Admin <Admin@DESKTOP-O22JR16>
2024-04-01 23:30:18 +0530

    changes done

--- src/main/java/com/contact/payload/Payload.java
+++ src/main/java/com/contact/payload/Payload.java
@@ -1,5 +1,10 @@
 package com.contact.payload;
 
 public class Payload {
+	private void sysout() {
+		// TODO Auto-generated method stub
+
+	}
+	
 
 }
--- src/main/java/com/contact/repo/Repo.java
+++ src/main/java/com/contact/repo/Repo.java
@@ -1,5 +1,9 @@
 package com.contact.repo;
 
 public class Repo {
+	private void sysout() {
+		// TODO Auto-generated method stub
+
+	}
 
 }

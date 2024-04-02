70dd8555d419aedc2c21735107440c932267db27
Admin <Admin@DESKTOP-O22JR16>
2024-04-02 22:06:35 +0530

    done with changes

--- src/main/java/com/contact/entities/Entity.java
+++ src/main/java/com/contact/entities/Entity.java
@@ -1,5 +1,18 @@
 package com.contact.entities;
 
 public class Entity {
+	
+	public static void name() {
+		
+		System.out.println("the given code is for only ");
+		System.out.println("the given code is for only ");
+		System.out.println("the given code is for only ");
+		System.out.println("the given code is for only ");
+
+		
+	}
+	
+	
+	
 
 }
